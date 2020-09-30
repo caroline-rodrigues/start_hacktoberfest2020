5e7f3d428ef6a9ab5d4385b56144d18ac8fc839b
rudrabarad <rudra.barad@gmail.com>
2020-10-01 00:43:33 +0530

    Done and Dusted :rocket:

---
+++ scripts/hello_world_rudrabarad.cpp
@@ -0,0 +1,18 @@
+// LANGUAGE: C++
+// ENV: 
+// AUTHOR: Rudra Barad
+// GITHUB: [Rudra Barad](https://github.com/rudrabarad)
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    std::cout << "By Rudra Barad";
+    return 0;
+}
+
+// I used this language as I am very familiar to this language and it is very powerful language for competitive coding
+/* Links to some C++ Tutorials 
+   - https://www.youtube.com/watch?v=j8nAHeVKL08&list=PLu0W_9lII9agpFUAlPFe_VNSlXW5uE0YL
+   - https://www.youtube.com/watch?v=vLnPwxZdW4Y
+*/
