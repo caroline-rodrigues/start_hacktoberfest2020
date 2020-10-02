9786185b0e01e6899ce6568055353e6f9372331a
Atreyo69 <atreyoarka1235@gmail.com>
2020-10-02 16:35:24 +0530

    Added a cpp program to print hello world

---
+++ scripts/helloworld.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Atreyo Mandal
+// GITHUB: [Atreyo Mandal](https://github.com/Atreyo69)
+
+
+#include<iostream.h>
+using namespace std;
+int main() 
+{
+   cout << "Hello, World!, Atreyo Mandal" << endl; 
+   return 0;
+}
+// I used this language as I am used to this language and it is a powerful language for competitive coding, taught at our university
