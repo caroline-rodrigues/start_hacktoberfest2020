e3cb43ac0ef114309f5d559aa2ae25df4a4309f8
Abhishek Panwar <44024188+abhishekpanwar97@users.noreply.github.com>
2020-10-03 16:37:58 +0530

    Create hello_world_Abhishek_Panwar.cpp

---
+++ scripts/hello_world_Abhishek_Panwar.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: GCC
+// AUTHOR: Abhishek Panwar
+// GITHUB: https://github.com/abhishekpanwar97
+
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Hello World"<<endl;
+return 0;
+}
