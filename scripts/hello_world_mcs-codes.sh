6f30caf10a280f1557cd3ba0783b66b85cb182c0
Mcvean Soans <69250084+mcs-codes@users.noreply.github.com>
2020-10-04 08:15:53 +0530

    Completed Hello World in Bash

--- scripts/hello_world_mcs-codes.sh
+++ scripts/hello_world_mcs-codes.sh
@@ -6,4 +6,4 @@
 # she-bang to tell the OS to invoke the specified shell to execute the commands that follow in the script
 
 echo "Hello World"
-# Run or execute the script using "./hello.sh" OR "bash hello.sh" OR "sh hello.sh"
+# Run or execute the script using "./hello_world_mcs-codes.sh" OR "bash hello_world_mcs-codes.sh" OR "sh hello_world_mcs-codes.sh"
